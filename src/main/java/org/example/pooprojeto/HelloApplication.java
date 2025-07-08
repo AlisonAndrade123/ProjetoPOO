db07d6893558a3306424142eae003d30e31d268f
Alison Andrade Alves <alison.alves@academico.ifpb.edu.br>
2025-07-08 10:46:28 -0300

    Enviando fxmls2

--- src/main/java/org/example/pooprojeto/HelloApplication.java
+++ src/main/java/org/example/pooprojeto/HelloApplication.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("view7.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 680, 470);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("view5.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
